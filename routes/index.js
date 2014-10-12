69082a08825c089e5d69bd412ccbfe2df4156888
Farez Vadsaria <farezvad@gmail.com>
2014-10-12 16:20:55 -0400

    Added username route & stubbed main post function

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Steam Report Card' });
+});
+
+/* GET user page. */
+router.get('/:username?', function(req, res) {
+    res.render('user', { user: req.params.username });
+});
+
+router.post('/rc', function(req, res) {
+   if(!req.body.username) {
+       res.render('error', {message: 'Please enter a valid username' });
+   } else {
+       // Search cache first
+
+       // If found, redirect
+
+       // Otherwise, make api call
+
+       // Store it in cache
+
+       // Redirect
+       res.redirect(req.body.username.toString());
+   }
 });
 
 module.exports = router;
